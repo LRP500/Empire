6b19dd78a2a931070c1b8ba96bb5d2078a3ff020
Pierre Roy <lrp.roy@gmail.com>
2020-05-29 00:37:23 +0200

    authorities manager

---
+++ Assets/_Empire/Scripts/Gameplay/Managers/AuthoritiesManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Empire
+{
+    [CreateAssetMenu(menuName = "Empire/Managers/Authorities Manager")]
+    public class AuthoritiesManager : ScriptableObject
+    {
+        public void Initialize()
+        {
+        }
+
+        private void RefreshOnTick(float elapsed)
+        {
+        }
+
+        private void Refresh()
+        {
+        }
+    }
+}
