d4115142871e8a88d011e2c8c963899612904d2b
Pierre Roy <lrp.roy@gmail.com>
2020-10-21 23:16:00 +0200

    Screen shake on cash spent and failed take over

---
+++ Assets/_Empire/Scripts/Camera/CameraImpulseManager.cs
@@ -0,0 +1,39 @@
+﻿using Cinemachine;
+using Sirenix.OdinInspector;
+using Tools;
+using UnityEngine;
+
+namespace Empire
+{
+    /// <summary>
+    /// Generate camera impulses on events received.
+    /// </summary>
+    [RequireComponent(typeof(CinemachineImpulseSource))]
+    public class CameraImpulseManager : MonoBehaviour
+    {
+        private CinemachineImpulseSource _impulseSource = null;
+
+        private void Awake()
+        {
+            _impulseSource = GetComponent<CinemachineImpulseSource>();
+
+            EventManager.Instance.Subscribe(GameplayEvent.TakeOverFailed, OnEventReceived);
+            EventManager.Instance.Subscribe(GameplayEvent.CashSpent, OnEventReceived);
+        }
+
+        [Button]
+        private void GenerateImpulse()
+        {
+#if UNITY_EDITOR
+            GetComponent<CinemachineImpulseSource>().GenerateImpulse();
+#else
+            _impulseSource.GenerateImpulse();
+#endif
+        }
+
+        private void OnEventReceived(object parameter)
+        {
+            GenerateImpulse();
+        }
+    }
+}
