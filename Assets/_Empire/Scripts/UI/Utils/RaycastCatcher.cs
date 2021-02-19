b0f1c3415de93b3659c8fa1ddc063f446c15bc06
Pierre Roy <lrp.roy@gmail.com>
2021-02-19 23:55:07 +0100

    Raycast catcher failed

---
+++ Assets/_Empire/Scripts/UI/Utils/RaycastCatcher.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Empire
+{
+    public class RaycastCatcher : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+        }
+    }
+}
